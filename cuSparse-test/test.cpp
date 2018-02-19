bd38d1c1641e057645a53d6eee115244fa15f04a
Jieyang Chen <cjy7117@gmail.com>
2018-02-19 00:46:39 -0800

    test

--- cuSparse-test/test.cpp
+++ cuSparse-test/test.cpp
@@ -59,8 +59,7 @@ int main(){
 	double ** cooValHostPtr = new double * [deviceCount];
 
 	// CPU x
-	int * xIndHostPtr;
-	double * xValHostPtr;
+	double * xHostPtr;
 
 	// CPU y
 	double * yHostPtr;
@@ -71,10 +70,9 @@ int main(){
 	double ** cooVal = new double * [deviceCount];
 	int ** csrRowPtr = new int * [deviceCount];
 
-	// GPU x
-	int ** xInd = new int * [deviceCount];
-	double ** xVal = new double * [deviceCount];
-	
+	// CPU x
+	double ** x = new double * [deviceCount];
+
 	// GPU y
 	double ** y = new double * [deviceCount];
 	
@@ -139,14 +137,14 @@ int main(){
 			}
 		}
 
+
+
 		if (d == 0)
-		{
-			nnz_vector = n; 
-			xIndHostPtr = (int *) malloc(nnz_vector*sizeof(int)); 
-			xValHostPtr = (double *)malloc(nnz_vector*sizeof(double)); 
+		{		
+			xHostPtr = (double *)malloc(n * sizeof(double)); 
 			yHostPtr = (double *)malloc(n * sizeof(double)); 
 
-			if((!xIndHostPtr) || (!xValHostPtr) || (!yHostPtr) || (!zHostPtr))
+			if((!yHostPtr) || (!xHostPtr))
 			{ 
 				CLEANUP("Host malloc failed (vectors)"); 
 				return 1; 
@@ -154,9 +152,8 @@ int main(){
 
 			for (int i = 0; i < n; i++)
 			{
-				xIndHostPtr[i] = i; 
-				xValHostPtr[i] = ((double) rand() / (RAND_MAX)); 
-				yHostPtr[i] = ((double) rand() / (RAND_MAX));
+				xHostPtr[i] = ((double) rand() / (RAND_MAX)); 
+				yHostPtr[i] = 0.0;
 			}
 
 		}
@@ -165,14 +162,12 @@ int main(){
 		cudaStat2[d] = cudaMalloc((void**)&cooColIndex[dd],nnz[d]*sizeof(int)); 
 		cudaStat3[d] = cudaMalloc((void**)&cooVal[d], nnz[d]*sizeof(double)); 
 		cudaStat4[d] = cudaMalloc((void**)&y[d], nb*sizeof(double)); 
-		cudaStat5[d] = cudaMalloc((void**)&xInd[d],nnz_vector*sizeof(int)); 
-		cudaStat6[d] = cudaMalloc((void**)&xVal[d],nnz_vector*sizeof(double)); 
+		cudaStat5[d] = cudaMalloc((void**)&x[d], n*sizeof(int)); 
 		if ((cudaStat1 != cudaSuccess) || 
 			(cudaStat2 != cudaSuccess) || 
 			(cudaStat3 != cudaSuccess) || 
 			(cudaStat4 != cudaSuccess) || 
-			(cudaStat5 != cudaSuccess) || 
-			(cudaStat6 != cudaSuccess)) 
+			(cudaStat5 != cudaSuccess)) 
 		{ 
 			CLEANUP("Device malloc failed");
 			return 1; 
@@ -188,21 +183,17 @@ int main(){
 							  (size_t)(nnz[d]*sizeof(double)), 
 							  cudaMemcpyHostToDevice); 
 		cudaStat4 = cudaMemcpy(y[d], yHostPtr + d * nb, 
-							  (size_t)(n*sizeof(double)), 
+							  (size_t)(nb*sizeof(double)), 
 							  cudaMemcpyHostToDevice); 
-		cudaStat5 = cudaMemcpy(xInd[d], xIndHostPtr[d], 
-							  (size_t)(nnz_vector*sizeof(int])), 
-							  cudaMemcpyHostToDevice); 
-		cudaStat6 = cudaMemcpy(xVal[d], xValHostPtr[d], 
-							  (size_t)(nnz_vector*sizeof(double)), 
+		cudaStat5 = cudaMemcpy(x[d], xHostPtr, 
+							  (size_t)(n*sizeof(int])), 
 							  cudaMemcpyHostToDevice); 
 
 		if ((cudaStat1 != cudaSuccess) ||
 		 	(cudaStat2 != cudaSuccess) ||
 		  	(cudaStat3 != cudaSuccess) ||
 		   	(cudaStat4 != cudaSuccess) ||
-		    (cudaStat5 != cudaSuccess) ||
-		    (cudaStat6 != cudaSuccess)) 
+		    (cudaStat5 != cudaSuccess)) 
 		{ 
 			CLEANUP("Memcpy from Host to Device failed"); 
 			return 1; 
@@ -235,94 +226,53 @@ int main(){
 		cusparseSetMatType(descr[d],CUSPARSE_MATRIX_TYPE_GENERAL); 
 		cusparseSetMatIndexBase(descr[d],CUSPARSE_INDEX_BASE_ZERO); 
 
+		/* exercise conversion routines (convert matrix from COO 2 CSR format) */ 
+		cudaStat1 = cudaMalloc((void**)&csrRowPtr[d],(nb+1)*sizeof(int)); 
+		if (cudaStat1 != cudaSuccess) 
+		{ 
+			CLEANUP("Device malloc failed (csrRowPtr)"); 
+			return 1; 
+		} 
 
+		status= cusparseXcoo2csr(handle,
+								cooRowIndex[d],nnz[d],nb, 
+								csrRowPtr[d],CUSPARSE_INDEX_BASE_ZERO);
+		if (status != CUSPARSE_STATUS_SUCCESS) 
+		{ 
+			CLEANUP("Conversion from COO to CSR format failed"); 
+			return 1; 
+		} 
+
+
+		cudaEvent_t start, stop;
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+
+		cudaEventRecord(start);
+		for (int i = 0; i < 10; i++) 
+		{
+			status = cusparseDcsrmv(handle,CUSPARSE_OPERATION_NON_TRANSPOSE, 
+									nb, n, nnz[d], 
+									&dtwo, descr[d], cooVal[d], 
+									csrRowPtr[d], cooColIndex[d], 
+									&x[d], &dthree, &y[d]); 
+		
+		}
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
+		float milliseconds = 0;
+		cudaEventElapsedTime(&milliseconds, start, stop);
+		printf("cusparseDcsrmv time = %f\n", milliseconds);
+		long long flop = nnz * 2;
+		double gflops = (flop / (milliseconds/1000))/1000000000;
+		printf("GFLOPS = %f\n", gflops);
 	 
 	}
 	
 	
+
 	
 	
-	
-	
-	
-	// /* allocate GPU memory and copy the matrix and vectors into it */ 
-	// 
-
-
-	// /* initialize cusparse library */ 
-	// status= cusparseCreate(&handle); 
-	// if (status != CUSPARSE_STATUS_SUCCESS) 
-	// { 
-	// 	CLEANUP("CUSPARSE Library initialization failed");
-	// 	return 1; 
-	// } 
-
-	// /* create and setup matrix descriptor */ 
-	// status= cusparseCreateMatDescr(&descr);
-	// if (status != CUSPARSE_STATUS_SUCCESS) 
-	// { 
-	// 	CLEANUP("Matrix descriptor initialization failed");
-	// 	return 1;
-	// } 
-
-	// cusparseSetMatType(descr,CUSPARSE_MATRIX_TYPE_GENERAL); 
-	// cusparseSetMatIndexBase(descr,CUSPARSE_INDEX_BASE_ZERO); 
-
-	// /* exercise conversion routines (convert matrix from COO 2 CSR format) */ 
-	// cudaStat1 = cudaMalloc((void**)&csrRowPtr,(n+1)*sizeof(csrRowPtr[0])); 
-	// if (cudaStat1 != cudaSuccess) 
-	// { 
-	// 	CLEANUP("Device malloc failed (csrRowPtr)"); 
-	// 	return 1; 
-	// } 
-
-	// status= cusparseXcoo2csr(handle,
-	// 						cooRowIndex,nnz,n, 
-	// 						csrRowPtr,CUSPARSE_INDEX_BASE_ZERO);
-	// if (status != CUSPARSE_STATUS_SUCCESS) 
-	// { 
-	// 	CLEANUP("Conversion from COO to CSR format failed"); 
-	// 	return 1; 
-	// } 
-
-	// //csrRowPtr = [0 3 4 7 9] 
-	// // The following test only works for compute capability 1.3 and above 
-	// // because it needs double precision. 
-	// int devId; 
-	// cudaDeviceProp prop; 
-	// cudaError_t cudaStat; 
-	// cudaStat = cudaGetDevice(&devId); 
-	// if (cudaSuccess != cudaStat)
-	// { 
-	// 	CLEANUP("cudaGetDevice failed"); 
-	// 	printf("Error: cudaStat %d, %s\n", cudaStat, cudaGetErrorString(cudaStat));
-	// 	return 1;
-	// } 
-	// cudaStat = cudaGetDeviceProperties( &prop, devId);
-	// if (cudaSuccess != cudaStat)
-	// { 
-	// 	CLEANUP("cudaGetDeviceProperties failed"); 
-	// 	printf("Error: cudaStat %d, %s\n", cudaStat, cudaGetErrorString(cudaStat));
-	// 	return 1; 
-	// } 
-	// int cc = 100*prop.major + 10*prop.minor; 
-	// if (cc < 130)
-	// { 
-	// 	CLEANUP("waive the test because only sm13 and above are supported\n"); 
-	// 	printf("the device has compute capability %d\n", cc); 
-	// 	printf("example test WAIVED"); 
-	// 	return 2; 
-	// } 
-	// /* exercise Level 1 routines (scatter vector elements) */ 
-	// status = cusparseDsctr(handle, 
-	// 					nnz_vector, xVal, xInd, 
-	// 					&y[n], CUSPARSE_INDEX_BASE_ZERO);
-	// if (status != CUSPARSE_STATUS_SUCCESS) 
-	// { 
-	// 	CLEANUP("Scatter from sparse to dense vector failed"); 
-	// 	return 1; 
-	// } 
-
 	// //y = [10 20 30 40 | 100 200 70 400] 
 	// /* exercise Level 2 routines (csrmv) */
 
