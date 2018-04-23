8a597e80945d934688b2fc9aa1dbd05f17d16d44
Jieyang Chen <cjy7117@gmail.com>
2018-04-23 09:42:27 -0700

    test

--- cuSparse-test/test2.cpp
+++ cuSparse-test/test2.cpp
@@ -330,19 +330,144 @@ int test(int iban_gpus, double  r11, double r22){
               int m, int n, int nnz, double * alpha, 
               double * csrValA, int * csrRowPtrA, int * csrColIndA,
               double * x, double * beta, 
-              double *y,
+              double * y,
               int ngpu){
 		int * start_idx = new int[ngpu];
 		int * end_idx   = new int[ngpu];
 		int * start_row = new int[ngpu];
 		int * end_row   = new int[ngpu];
 
+		int curr_row;
+
+		double **    csrValA_partial = new double * [ngpu];
+		int    ** csrRowPtrA_partial = new int    * [ngpu];
+		int    ** csrColIndA_partial = new int    * [ngpu];
+		int    *         nnz_partial = new int      [ngpu];
+		int    *           m_partial = new int    * [ngpu];
+		int    *           n_partial = new int    * [ngpu];
+
 		for (int i = 0; i < ngpu; i++) {
-			start_idx[i] = floor((i)     * nnz / ngpu);
-			end_idx[i]   = floor((i + 1) * nnz / ngpu) - 1;
+			start_idx[i]   = floor((i)     * nnz / ngpu);
+			end_idx[i]     = floor((i + 1) * nnz / ngpu) - 1;
+			nnz_partial[i] = end_idx[i] - start_idx[i] + 1;
 		}
 
+		curr_row = 0;
+		for (int i = 0; i < ngpu; i++) {
+			while (csrRowPtrA[curr_row] < start_idx[i]) curr_row++;
+			start_row[i] = curr_row - 1;
+		}
+
+		curr_row = 0;
+		for (int i = 0; i < ngpu; i++) {
+			while (csrRowPtrA[curr_row] < end_idx[i]) curr_row++;
+			end_row[i] = curr_row - 1;
+		}
+
+		for (int i = 0; i < ngpu; i++) {
+			m_partial[i] = end_row[i] - start_row[i] + 1;
+			n_partial[i] = n;
+		}
+
+		for (int i = 0; i < ngpu; i++) {
+			csrValA_partial[i] = new double[nnz_partial[i]];
+			memcpy((void *)csrValA_partial[i], 
+				   (void *)&csrValA[start_idx[i]], 
+				   sizeof(double) * nnz_partial[i]);
+		}
+
+		curr_row = 0;
+		for (int i = 0; i < ngpu; i++) {
+			csrRowPtrA_partial[i] = new int [m_partial[i] + 1];
+			csrRowPtrA_partial[i][0] = 0;
+			csrRowPtrA_partial[i][m_partial[i]] = nnz_partial[i];
+
+			for (int j = 1; j < m_partial[i]; i++) {
+				csrRowPtrA_partial[i][j] = csrRowPtrA[start_row[i] + j];
+			}
+			for (int j = 1; j < m_partial[i]; i++) {
+				csrRowPtrA_partial[i][j] -= start_idx[i];
+			}
+		}
+
+		for (int i = 0; i < ngpu; i++) {
+			csrColIndA_partial[i] = new double[nnz_partial[i]];
+			memcpy((void *)csrColIndA_partial[i], 
+				   (void *)&csrColIndA[start_idx[i]], 
+				   sizeof(double) * nnz_partial[i]);
+		}
+
+		cudaStream_t       * stream = new cudaStream_t [deviceCount];
+		cusparseStatus_t   * status = new cusparseStatus_t[ngpu];
+		cusparseHandle_t   * handle = new cusparseHandle_t[ngpu];
+		cusparseMatDescr_t * descr  = new cusparseMatDescr_t[ngpu];
 		
+		for (int d = 0; d < ngpu; d++) {
+
+			cudaSetDevice(d);
+ 			cudaStreamCreate(&(stream[d]));
+
+			status[d] = cusparseCreate(&(handle[d])); 
+			if (status[d] != CUSPARSE_STATUS_SUCCESS) 
+			{ 
+				CLEANUP("CUSPARSE Library initialization failed");
+				return 1; 
+			} 
+			status[d] = cusparseSetStream(handle[d], stream[d]);
+			if (status[d] != CUSPARSE_STATUS_SUCCESS) 
+			{ 
+				CLEANUP("Stream bindind failed");
+				return 1;
+			} 
+			status[d] = cusparseCreateMatDescr(&descr[d]);
+			if (status[d] != CUSPARSE_STATUS_SUCCESS) 
+			{ 
+				CLEANUP("Matrix descriptor initialization failed");
+				return 1;
+			} 	
+			cusparseSetMatType(descr[d],CUSPARSE_MATRIX_TYPE_GENERAL); 
+			cusparseSetMatIndexBase(descr[d],CUSPARSE_INDEX_BASE_ZERO);
+		}
+
+		double **    d_csrValA_partial = new double * [ngpu];
+		int    ** d_csrRowPtrA_partial = new int    * [ngpu];
+		int    ** d_csrColIndA_partial = new int    * [ngpu];
+
+		for (int i = 0; i < ngpu; i++) {
+			cudaSetDevice(d);
+			cudaMalloc((void**)d_csrValA_partial[i],    nnz_partial[i]     * sizeof(double));
+			cudaMalloc((void**)d_csrRowPtrA_partial[i], (m_partial[i] + 1) * sizeof(int)   );
+			cudaMalloc((void**)d_csrColIndA_partial[i], nnz_partial[i]     * sizeof(int)   );
+		}
+
+		int repeat_test = 10;
+		double start = get_time();
+		for (int i = 0; i < repeat_test; i++) 
+		{
+			for (int d = 0; d < ngpu; ++d) 
+			{
+				cudaSetDevice(d);				
+				status[d] = cusparseDcsrmv(handle[d],CUSPARSE_OPERATION_NON_TRANSPOSE, 
+											m_partial[d], n_partial[d], nnz_partial[d], 
+											alpha, descr[d], d_csrValA_partial[d], 
+											d_csrRowPtrA_partial[d], d_csrColIndA_partial[d], 
+											x[d],  beta, y[d]); 
+			}
+			for (int d = 0; d < deviceCount; ++d) 
+			{
+				cudaSetDevice(d);
+				cudaDeviceSynchronize();
+			}
+		}
+		double end = get_time();
+
+		cout << start << "  " << end << endl;
+
+		double time = end - start;
+
+		printf("cusparseDcsrmv time = %f s\n", time);
+
+
 		
 
 
