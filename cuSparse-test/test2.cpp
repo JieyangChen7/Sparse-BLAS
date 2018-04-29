5aff3c659d17666ec558e9d14297f5a204573e1c
Jieyang Chen <cjy7117@gmail.com>
2018-04-29 00:32:57 -0700

    test

--- cuSparse-test/test2.cpp
+++ cuSparse-test/test2.cpp
@@ -586,7 +586,7 @@ int spMV_mgpu_v1(int m, int n, int nnz, double * alpha,
 			tmp = get_time();
 			cudaSetDevice(d);
 			//cout << "dev_m[d]: " << dev_m[d] << ", dev_n[d]: " << dev_n[d] << ", dev_nnz[d]: " << dev_nnz[d] << endl;
-			status[d] = cusparseDcsrmv(handle[d],CUSPARSE_OPERATION_NON_TRANSPOSE, 
+			status[d] = cusparseDcsrmv_mp(handle[d],CUSPARSE_OPERATION_NON_TRANSPOSE, 
 									   dev_m[d], dev_n[d], dev_nnz[d], 
 									   alpha, descr[d], dev_csrVal[d], 
 									   dev_csrRowPtr[d], dev_csrColIndex[d], 
@@ -934,7 +934,7 @@ int spMV_mgpu_v2(int m, int n, int nnz, double * alpha,
 				cudaDeviceSynchronize();
 				cout << "computation " << d << " : " << get_time()-tmp << endl;
 
-				//print_error(status[d]);
+				print_error(status[d]);
 				
 			}
 			for (int d = 0; d < ngpu; ++d) 
