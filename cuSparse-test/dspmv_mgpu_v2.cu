fa2c3bd2f1e34ee0ff1d343622b4a338b505fe2e
Jieyang Chen <cjy7117@gmail.com>
2018-06-03 14:58:55 -0700

    test

--- cuSparse-test/dspmv_mgpu_v2.cu
+++ cuSparse-test/dspmv_mgpu_v2.cu
@@ -137,8 +137,8 @@ int spMV_mgpu_v2(int m, int n, int nnz, double * alpha,
 				curr_spmv_task->dev_y = dev_y[c];
 
 				assign_task(curr_spmv_task, dev_id, stream);
-				// run_task(curr_spmv_task, dev_id, handle, kernel);
-				// finalize_task(curr_spmv_task, dev_id, stream);
+				run_task(curr_spmv_task, dev_id, handle, kernel);
+				finalize_task(curr_spmv_task, dev_id, stream);
 
 			} else {
 				break;
