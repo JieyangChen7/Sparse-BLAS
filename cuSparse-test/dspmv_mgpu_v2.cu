4ff7e838d88643b8b0fafac24b7cabc45687abd7
Jieyang Chen <cjy7117@gmail.com>
2018-06-04 13:58:20 -0700

    test

--- cuSparse-test/dspmv_mgpu_v2.cu
+++ cuSparse-test/dspmv_mgpu_v2.cu
@@ -191,60 +191,60 @@ void generate_tasks(int m, int n, int nnz, double * alpha,
 		}
 	}
 
-	// curr_row = 0;
-	// for (t = 0; t < num_of_tasks; t++) {
-	// 	spmv_task_pool[t].end_row = get_row_from_index(m, csrRowPtr, spmv_task_pool[t].end_idx);
+	curr_row = 0;
+	for (t = 0; t < num_of_tasks; t++) {
+		spmv_task_pool[t].end_row = get_row_from_index(m, csrRowPtr, spmv_task_pool[t].end_idx);
 
-	// 	// Mark imcomplete rows
-	// 	// True: imcomplete
-	// 	if (spmv_task_pool[t].end_idx < csrRowPtr[spmv_task_pool[t].end_row + 1] - 1)  {
-	// 		spmv_task_pool[t].end_flag = true;
-	// 	} else {
-	// 		spmv_task_pool[t].end_flag = false;
-	// 	}
-	// }
+		// Mark imcomplete rows
+		// True: imcomplete
+		if (spmv_task_pool[t].end_idx < csrRowPtr[spmv_task_pool[t].end_row + 1] - 1)  {
+			spmv_task_pool[t].end_flag = true;
+		} else {
+			spmv_task_pool[t].end_flag = false;
+		}
+	}
 
-	// // Cacluclate dimensions
-	// for (t = 0; t < num_of_tasks; t++) {
-	// 	spmv_task_pool[t].dev_m = spmv_task_pool[t].end_row - spmv_task_pool[t].start_row + 1;
-	// 	spmv_task_pool[t].dev_n = n;
-	// }
+	// Cacluclate dimensions
+	for (t = 0; t < num_of_tasks; t++) {
+		spmv_task_pool[t].dev_m = spmv_task_pool[t].end_row - spmv_task_pool[t].start_row + 1;
+		spmv_task_pool[t].dev_n = n;
+	}
 
-	// for (t = 0; t < num_of_tasks; t++) {
-	// 	spmv_task_pool[t].host_csrRowPtr = new int [spmv_task_pool[t].dev_m + 1];
-	// 	spmv_task_pool[t].host_csrRowPtr[0] = 0;
-	// 	spmv_task_pool[t].host_csrRowPtr[spmv_task_pool[t].dev_m] = spmv_task_pool[t].dev_nnz;
+	for (t = 0; t < num_of_tasks; t++) {
+		spmv_task_pool[t].host_csrRowPtr = new int [spmv_task_pool[t].dev_m + 1];
+		spmv_task_pool[t].host_csrRowPtr[0] = 0;
+		spmv_task_pool[t].host_csrRowPtr[spmv_task_pool[t].dev_m] = spmv_task_pool[t].dev_nnz;
 
-	// 	memcpy(&(spmv_task_pool[t].host_csrRowPtr[1]), 
-	// 		   &csrRowPtr[spmv_task_pool[t].start_row + 1], 
-	// 		   (spmv_task_pool[t].dev_m - 1) * sizeof(int) );
+		memcpy(&(spmv_task_pool[t].host_csrRowPtr[1]), 
+			   &csrRowPtr[spmv_task_pool[t].start_row + 1], 
+			   (spmv_task_pool[t].dev_m - 1) * sizeof(int) );
 
-	// 	for (int j = 1; j < spmv_task_pool[t].dev_m; j++) {
-	// 		spmv_task_pool[t].host_csrRowPtr[j] -= spmv_task_pool[t].start_idx;
-	// 	}
+		for (int j = 1; j < spmv_task_pool[t].dev_m; j++) {
+			spmv_task_pool[t].host_csrRowPtr[j] -= spmv_task_pool[t].start_idx;
+		}
 
-	// 	spmv_task_pool[t].host_csrColIndex = csrColIndex;
-	// 	spmv_task_pool[t].host_csrVal = csrVal;
-	// 	spmv_task_pool[t].host_y = y;
-	// 	spmv_task_pool[t].host_x = x;
-	// 	spmv_task_pool[t].local_result_y = new double[spmv_task_pool[t].dev_m];
-	// 	spmv_task_pool[t].alpha = new double[1];
-	// 	spmv_task_pool[t].beta = new double[1]; 
-	// 	spmv_task_pool[t].alpha[0] = *alpha;
-	// 	spmv_task_pool[t].beta[0] = *beta;
+		spmv_task_pool[t].host_csrColIndex = csrColIndex;
+		spmv_task_pool[t].host_csrVal = csrVal;
+		spmv_task_pool[t].host_y = y;
+		spmv_task_pool[t].host_x = x;
+		// spmv_task_pool[t].local_result_y = new double[spmv_task_pool[t].dev_m];
+		spmv_task_pool[t].alpha = new double[1];
+		spmv_task_pool[t].beta = new double[1]; 
+		spmv_task_pool[t].alpha[0] = *alpha;
+		spmv_task_pool[t].beta[0] = *beta;
 
-	// }
+	}
 
-	// for (t = 0; t < num_of_tasks; t++) {
-	// 	cusparseStatus_t status = cusparseCreateMatDescr(&(spmv_task_pool[t].descr));
-	// 	if (status != CUSPARSE_STATUS_SUCCESS) 
-	// 	{ 
-	// 		printf("Matrix descriptor initialization failed");
-	// 		//return 1;
-	// 	} 	
-	// 	cusparseSetMatType(spmv_task_pool[t].descr,CUSPARSE_MATRIX_TYPE_GENERAL); 
-	// 	cusparseSetMatIndexBase(spmv_task_pool[t].descr,CUSPARSE_INDEX_BASE_ZERO);
-	// }
+	for (t = 0; t < num_of_tasks; t++) {
+		cusparseStatus_t status = cusparseCreateMatDescr(&(spmv_task_pool[t].descr));
+		if (status != CUSPARSE_STATUS_SUCCESS) 
+		{ 
+			printf("Matrix descriptor initialization failed");
+			//return 1;
+		} 	
+		cusparseSetMatType(spmv_task_pool[t].descr,CUSPARSE_MATRIX_TYPE_GENERAL); 
+		cusparseSetMatIndexBase(spmv_task_pool[t].descr,CUSPARSE_INDEX_BASE_ZERO);
+	}
 
 	// for (t = 0; t < num_of_tasks; t++) {
 	// 	(*spmv_task_pool_ptr).push_back(&spmv_task_pool[t]);
