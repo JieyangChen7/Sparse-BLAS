5969d3bdc90158632863f4f686f11bc1b1ef51ae
Jieyang Chen <cjy7117@gmail.com>
2018-07-19 15:52:51 -0700

    test

--- SparseBLAS/src/dspmv_mgpu_v2.cu
+++ SparseBLAS/src/dspmv_mgpu_v2.cu
@@ -74,7 +74,7 @@ int spMV_mgpu_v2(int m, int n, int nnz, double * alpha,
 		
 		int c;
 		unsigned int dev_id = omp_get_thread_num();
-		cout << "thread " << dev_id << "started" << endl;
+		cout << "thread " << dev_id <<"/" << omp_get_num_threads() << "started" << endl;
 		cudaSetDevice(dev_id);
 		
 		cusparseStatus_t status[copy_of_workspace];
