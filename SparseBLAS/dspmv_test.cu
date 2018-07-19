3d34896de4cc58c60a7db5893908743823a55fa7
Jieyang Chen <cjy7117@gmail.com>
2018-07-19 09:46:27 -0700

    test

--- SparseBLAS/dspmv_test.cu
+++ SparseBLAS/dspmv_test.cu
@@ -388,7 +388,7 @@ int main(int argc, char *argv[]) {
 	avg_time_v1/=repeat_test;
 	avg_time_v2/=repeat_test;
 
-	cout << "================================Average===============================" << endl;
+	cout << "................................Average..............................." << endl;
 
 
 	cout << setw(11) << avg_time_baseline;
