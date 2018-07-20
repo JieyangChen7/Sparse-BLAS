21545f666925d53a1f78c42ed8fcf5703cabf48a
Jieyang Chen <cjy7117@gmail.com>
2018-07-20 16:21:29 -0700

    test

--- SparseBLAS/test/dspmv_test.cu
+++ SparseBLAS/test/dspmv_test.cu
@@ -338,7 +338,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	
-	for (int d = max(1, (int)ceil(matrix_size_in_gb / (get_gpu_availble_mem(ngpu) * 0.8))); d <= ngpu; d++) {
+	for (int d = 1; d <= ngpu; d*=2) {
 		for (int c = 1; c <= 8; c*=2) {
 			cout << "d = " << d << ", c = " << c << endl;
 			curr_time = get_time();
