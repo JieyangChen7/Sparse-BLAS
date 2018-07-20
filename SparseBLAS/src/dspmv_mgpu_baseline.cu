4ac92bcb825294c51d999303043f9126b8b2fbe9
Jieyang Chen <cjy7117@gmail.com>
2018-07-20 14:46:10 -0700

    test

--- SparseBLAS/src/dspmv_mgpu_baseline.cu
+++ SparseBLAS/src/dspmv_mgpu_baseline.cu
@@ -73,7 +73,11 @@ int spMV_mgpu_baseline(int m, int n, long long nnz, double * alpha,
 		dev_n[d]   = n;
 		dev_nnz[d] = (int)(csrRowPtr[end_row[d] + 1] - csrRowPtr[start_row[d]]);
 
-		long long matrix_data_space = dev_nnz[d] * sizeof(double) + dev_nnz[d] * sizeof(int) + (dev_m[d]+1) * sizeof(int);
+		long long matrix_data_space = dev_nnz[d] * sizeof(double) + 
+										dev_nnz[d] * sizeof(int) + 
+										(dev_m[d]+1) * sizeof(int) + 
+										dev_n[d] * sizeof(double) +
+										dev_m[d] * sizeof(double);
 		double matrix_size_in_gb = (double)matrix_data_space / 1e9;
 		if (matrix_size_in_gb > 0.8 * get_gpu_availble_mem(ngpu)) {
 			return -1;
