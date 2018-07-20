a1ae37b947a8c02ceeb1e12f8e1c6604ccfa0738
Jieyang Chen <cjy7117@gmail.com>
2018-07-20 16:02:13 -0700

    test

--- SparseBLAS/test/dspmv_test.cu
+++ SparseBLAS/test/dspmv_test.cu
@@ -391,12 +391,12 @@ int main(int argc, char *argv[]) {
 
 
 		curr_time = get_time();
-		ret2 = spMV_mgpu_v1(m, n, nnz, &ALPHA,
-					 cooVal, csrRowPtr, cooColIndex, 
-					 x, &BETA,
-					 y2,
-					 ngpu,
-					 kernel_version);
+		// ret2 = spMV_mgpu_v1(m, n, nnz, &ALPHA,
+		// 			 cooVal, csrRowPtr, cooColIndex, 
+		// 			 x, &BETA,
+		// 			 y2,
+		// 			 ngpu,
+		// 			 kernel_version);
 		time_v1 = get_time() - curr_time;	
 		
 		//cudaProfilerStart();
