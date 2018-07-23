745bac3eb204150031a5e03a01d5331dadd6d1bf
Jieyang Chen <cjy7117@gmail.com>
2018-07-23 11:22:11 -0700

    test

--- SparseBLAS/test/batch_test.sh
+++ SparseBLAS/test/batch_test.sh
@@ -3,9 +3,9 @@
 NGPU=8
 KERNEL=1
 
-./spmv g 10000 $(NGPU) 10 $(KERNEL)
-./spmv g 20000 $(NGPU) 10 $(KERNEL)
-./spmv g 30000 $(NGPU) 10 $(KERNEL)
-./spmv g 100000 $(NGPU) 10 $(KERNEL)
-./spmv g 200000 $(NGPU) 10 $(KERNEL)
-./spmv g 300000 $(NGPU) 10 $(KERNEL)
+./spmv g 10000 $NGPU 10 $KERNEL
+./spmv g 20000 $NGPU 10 $KERNEL
+./spmv g 30000 $NGPU 10 $KERNEL
+./spmv g 100000 $NGPU 10 $KERNEL
+./spmv g 200000 $NGPU 10 $KERNEL
+./spmv g 300000 $NGPU 10 $KERNEL
